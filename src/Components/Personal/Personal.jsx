10ea2b677db2f74980a23e9ee06b168827cdb78e
Olesyahristenko <Olesyahristenkojob@yandex.ru>
2023-07-24 17:56:39 +0300

    Форма в лк часть 1

--- src/Components/Personal/Personal.jsx
+++ src/Components/Personal/Personal.jsx
@@ -177,10 +177,235 @@ export default function Personal() {
         <a href="#">Просмотр инструкции к api</a>
       </div>
 
-
+<div className="request-report__wrapper">
+        <div className="request-report__description">
+          Уникальный номер протокола формируется по номеру объекта, лаб.номеру и
+          типу испытания, поэтому для разных протоколов эти параметры должны
+          отличаться. Например, для одной пробы два разных протокола будут с
+          одинаковыми номерами объекта и лаб.номерами, но тип опыта нужно
+          вводить разный.
+        </div>
+        <form className="row g-3" id="request-report">
+          <div className="col-md-4">
+            <label for="inputObj">Объект *</label>
+            <div className="input-group has-validation">
+              <span className="input-group-text" id="inputGroupObjInfo">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-info-circle"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                  <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" />
+                </svg>
+                <div className="form-info-box">Внутренний номер объекта</div>
+              </span>
+              <input
+                type="text"
+                className="form-control"
+                id="inputObj"
+                name="inputObj"
+                placeholder="111-11"
+                required
+                aria-describedby="inputGroupObjInfo"
+              />
+              
+            </div>
+          </div>
+          <div className="col-md-4">
+            <label for="inputLabNo">Лаб.№ *</label>
+            <div className="input-group has-validation">
+              <span className="input-group-text" id="inputGroupLabInfo">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-info-circle"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                  <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" />
+                </svg>
+                <div className="form-info-box">Лабораторный номер пробы</div>
+              </span>
+              <input
+                type="text"
+                className="form-control"
+                id="inputLabNo"
+                name="inputLabNo"
+                placeholder="A1-1/AA"
+                required
+                aria-describedby="inputGroupLabInfo"
+              />
+              
+            </div>
+          </div>
+          <div className="col-md-4">
+            <label for="inputType">Тип испытания *</label>
+            <div className="input-group has-validation">
+              <span className="input-group-text" id="inputGroupTypeInfo">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-info-circle"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                  <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" />
+                </svg>
+                <div className="form-info-box">Тип испытания</div>
+              </span>
+              <input
+                type="text"
+                className="form-control"
+                id="inputType"
+                name="inputType"
+                placeholder="FC, FCE, вибро, консолидация и т.д."
+                required
+                aria-describedby="inputGroupTypeInfo"
+              />
+              
+            </div>
+          </div>
+          <div className="col-6">
+            <label for="inputParam_1">Параметр</label>
+            <input
+              type="text"
+              className="form-control"
+              id="inputParam_1"
+              name="inputParam_1"
+              placeholder="Дата выдачи протокола"
+              aria-describedby="validationFeedback"
+            />
+            <div className="invalid-feedback" id="validationFeedback">
+              Пожалуйста, заполните это поле.
+            </div>
+          </div>
+          <div className="col-6">
+            <label for="inputParam_1_val">Значение</label>
+            <input
+              type="text"
+              className="form-control"
+              id="inputParam_1_val"
+              name="inputParam_1"
+              placeholder="01.09.2022"
+              aria-describedby="validationFeedback"
+            />
+            <div className="invalid-feedback" id="validationFeedback">
+              Пожалуйста, заполните это поле.
+            </div>
+          </div>
+          <div className="col-6">
+            <input
+              type="text"
+              className="form-control"
+              id="inputParam_2"
+              name="inputParam_2"
+              placeholder=""
+              aria-describedby="validationFeedback"
+            />
+            <div className="invalid-feedback" id="validationFeedback">
+              Пожалуйста, заполните это поле.
+            </div>
+          </div>
+          <div className="col-6">
+            <input
+              type="text"
+              className="form-control"
+              id="inputParam_2_val"
+              name="inputParam_2"
+              placeholder=""
+              aria-describedby="validationFeedback"
+            />
+            <div className="invalid-feedback" id="validationFeedback">
+              Пожалуйста, заполните это поле.
+            </div>
+          </div>
+          <div className="col-6">
+            <input
+              type="text"
+              className="form-control"
+              id="inputParam_3"
+              name="inputParam_3"
+              placeholder=""
+              aria-describedby="validationFeedback"
+            />
+            
+          </div>
+          <div className="col-6">
+            <input
+              type="text"
+              className="form-control"
+              id="inputParam_3_val"
+              name="inputParam_3"
+              placeholder=""
+              aria-describedby="validationFeedback"
+            />
+            
+          </div>
+          <div className="request-form__actions">
+            <button
+              type="button"
+              className="btn btn-outline-secondary request-form__action"
+              id="request-form-add-btn"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-plus"
+                viewBox="0 0 16 16"
+              >
+                <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+              </svg>
+            </button>
+            <button
+              type="button"
+              className="btn btn-outline-secondary request-form__action"
+              id="request-form-delete-btn"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-dash"
+                viewBox="0 0 16 16"
+              >
+                <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z" />
+              </svg>
+            </button>
+          </div>
+          <div className="col-12">
+            <button
+              type="submit"
+              className="btn-out btn btn-success btn-lg w-100 w-lg-50 align-center"
+              id="request-report-submit-btn"
+              disabled
+            >
+              Отправить
+            </button>
+          </div>
+        </form>
+        <div className="request-report-succses" id="request-report-succses">
+          Данные успешно отправлены!
+          <div className="request-report-succses__sub">
+            Дождитесь загрузки QR-кода
+          </div>
+        </div>
+      </div>
       
 
       <br />
+
+      
     </>
   );
 }
