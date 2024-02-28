47efc0ad9a7acce69136cb0b5b435e4b7a17d889
Nick <tnick1502@mail.ru>
2024-02-28 14:47:44 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 docker system prune -a -f
 docker rm $(docker ps -a -q) -f
 docker rmi $(docker images -a -q) -f
-cd /root/report_autification_frontend
+cd /root/report_autification_front
 git pull
 sudo service docker restart
 docker-compose up
